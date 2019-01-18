1b91640f5e91fa0ac6d84e96fa2539141e6efece
xiaohp <xiaohp1993@gmail.com>
2019-01-18 10:45:53 +0800

    add demo

---
+++ server.js
@@ -0,0 +1,26 @@
+const Koa = require('koa');
+const mount = require('koa-mount');
+const graphqlHTTP = require('koa-graphql');
+const { buildSchema } = require('graphql');
+
+ // Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+ // The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => 'Hello Koa GraphQL!',
+};
+
+const app = new Koa();
+app.use(mount('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+})));
+
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
